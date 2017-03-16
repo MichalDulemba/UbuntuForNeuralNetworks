6e4ef90cb782a65c5f69e8939c6623a3c9658465
Michal <photocoffeeman@gmail.com>
2017-03-16 15:12:44 +0100

    remove old nvidia docker

--- installnvidiadocker.sh
+++ installnvidiadocker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sudo apt-get purge nvidia-docker
 wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
 sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
